aa6a9bfb866d23de6593d0f53ccb446da08ce9c5
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-04 22:05:31 +0300

    Реализация фикстуры БД

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
+from fixture.db import DbFixture
 
 fixture = None
 target = None
@@ -22,18 +23,22 @@ def load_config(file):
 def app(request):  # функция, инициализирующая фикстуру
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))["web"]
+    web_config = load_config(request.config.getoption("--target"))
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])  # создание фикстуры
-    # fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
+        fixture = Application(browser=browser, base_url=web_config['web']['baseUrl'])  # создание фикстуры
+    fixture.session.ensure_login(username=web_config['webAdmin']['username'], password=web_config['webAdmin']['password'])
     return fixture
 
 
-# @pytest.fixture(scope='session')
-# def orm(request):  # функция, инициализирующая ORM
-#     db_config = load_config(request.config.getoption("--target"))["db"]
-#     db_fixture = ORMFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
-#     return db_fixture
+@pytest.fixture(scope='session')
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    db_fixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+
+    def fin():
+        db_fixture.destroy()
+    request.addfinalizer(fin)
+    return db_fixture
 
 
 @pytest.fixture(scope='session', autouse=True)
