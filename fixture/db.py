c5583cddfadb7601f2ba3c9470ce6920a9f90a23
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-04 22:04:59 +0300

    Подключение БД, реализация метода get_projects_list получения проектов

---
+++ fixture/db.py
@@ -0,0 +1,29 @@
+import pymysql.cursors
+from model.project import Project
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password,
+                                          autocommit=True)  # autocommit = True - отключили кэширование БД (кэш после каждого запроса сбрасывается)
+
+    #  загружает список групп из БД
+    def get_projects_list(self):
+        project_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, name, status, view_state, description from mantis_project_table")
+            for row in cursor:
+                (id, name, status, view_status, description) = row
+                project_list += [Project(id=str(id), name=name, status=status, view_status=view_status, description=description)]
+        finally:
+            cursor.close()
+        return project_list
+
+    def destroy(self):
+        self.connection.close()
