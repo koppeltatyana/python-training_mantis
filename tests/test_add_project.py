f26689f61e98727540e579ce4b8af61d0218824b
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-05 12:52:53 +0300

    Реализация теста добавления проекта

---
+++ tests/test_add_project.py
@@ -0,0 +1,10 @@
+from model.project import Project
+
+
+def test_add_project(app, db, json_project):
+    new_project = json_project
+    old_projects_list = db.get_projects_list()
+    app.project.create(new_project)
+    new_projects_list = db.get_projects_list()
+    old_projects_list += [new_project]
+    assert sorted(old_projects_list, key=Project.id_or_max) == sorted(new_projects_list, key=Project.id_or_max)
