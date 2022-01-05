233380891ca8f6bab3d5bdd0a53eb9e54a478af5
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-05 13:12:59 +0300

    Добавляем проект в список старых проектов только если этого проекта нет в списке

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -6,5 +6,6 @@ def test_add_project(app, db, json_project):
     old_projects_list = db.get_projects_list()
     app.project.create(new_project)
     new_projects_list = db.get_projects_list()
-    old_projects_list += [new_project]
+    if new_project not in old_projects_list:
+        old_projects_list += [new_project]
     assert sorted(old_projects_list, key=Project.id_or_max) == sorted(new_projects_list, key=Project.id_or_max)
