6b16981ad172a421f29d254430924fc377367a0a
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-05 12:35:48 +0300

    Создание файла со статическими данными

---
+++ data/project.py
@@ -0,0 +1,6 @@
+from model.project import Project
+
+test_data = [
+    Project(name="name3", status="development", view_status="private", description="asdqwesdfsgdjfgjkdhf"),
+    Project(name="name4", status="release", view_status="public", description="jfhgkdjfnvsjiesdbdfsbkdjfb")
+]
