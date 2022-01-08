0f70bc069270959bd4cc2f7ff6782267bea986c9
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-08 21:08:44 +0300

    Реализована функция получения проектов через SOAP

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 
 class SoapHelper:
@@ -8,9 +9,17 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("https://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
         except WebFault:
             return False
+
+    def get_projects(self, config):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        projects_list = []
+        all_projects = client.service.mc_projects_get_user_accessible(config["webAdmin"]["username"], config["webAdmin"]["password"])
+        for project in all_projects:
+            projects_list += [Project(id=project.id, name=project.name, description=project.description, status=str(project.status.name))]
+        return list(projects_list)
