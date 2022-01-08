b05841e3626c33f7c416ab9f473464c5c973b4f9
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-08 21:09:27 +0300

    Для теста добавления проектов реализована проверка списков проектов через SOAP

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -1,11 +1,12 @@
 from model.project import Project
 
 
-def test_add_project(app, db, json_project):
+def test_add_project(app, config, json_project):
+    config = config
     new_project = json_project
-    old_projects_list = db.get_projects_list()
+    old_projects_list = app.soap.get_projects(config)
     app.project.create(new_project)
-    new_projects_list = db.get_projects_list()
+    new_projects_list = app.soap.get_projects(config)
     if new_project not in old_projects_list:
         old_projects_list += [new_project]
     assert sorted(old_projects_list, key=Project.id_or_max) == sorted(new_projects_list, key=Project.id_or_max)
