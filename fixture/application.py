dd9c2bd9e7caa514288c8b6d2bedceac4e8b6c27
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-08 21:07:42 +0300

    Удалены лишние комментарии

--- fixture/application.py
+++ fixture/application.py
@@ -38,11 +38,5 @@ class Application:
         wd = self.wd  # получаем ссылку на драйвер из текущего объекта
         wd.get(self.base_url)
 
-
-    # def return_to_home_page(self):
-    #     wd = self.wd
-    #     if not (wd.current_url.endswith("/mantisbt-1.2.20/") and len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
-    #         wd.find_element_by_link_text("home").click()
-
     def destroy(self):
         self.wd.quit()
